e59db869e92b782ef4012b649b1d68a50deb9360
mychidarko <mickdd22@gmail.com>
2023-06-14 14:10:42 +0000

    feat: add support for attachments

--- src/Mail.php
+++ src/Mail.php
@@ -11,6 +11,7 @@ class Mail
 
     public function __construct($mail = null)
     {
+        unset($mail['attachments']);
         $this->mail = $mail;
     }
 
@@ -32,10 +33,46 @@ class Mail
             'senderEmail' => $this->mail['senderEmail'] ?? null,
             'replyToName' => $this->mail['replyToName'] ?? null,
             'replyToEmail' => $this->mail['replyToEmail'] ?? null,
-            'attachment' => $this->mail['attachment'] ?? '',
+            'attachments' => $this->mail['attachments'] ?? null,
             'cc' => $this->mail['cc'] ?? '',
             'bcc' => $this->mail['bcc'] ?? '',
         ];
+    }
+
+    /**
+     * Add attachments to your mail from your file system
+     * 
+     * @throws Exception
+     * @return Mail
+     */
+    public function attach(
+        $path,
+        $name = "",
+        $encoding = \PHPMailer\PHPMailer\PHPMailer::ENCODING_BASE64,
+        $type = "",
+        $disposition = "attachment"
+    ) {
+        if (is_array($path)) {
+            foreach ($path as $attachment) {
+                $this->mail['attachments'][] = [
+                    'path' => $attachment,
+                    'name' => $name,
+                    'encoding' => $encoding,
+                    'type' => $type,
+                    'disposition' => $disposition,
+                ];
+            }
+        } else {
+            $this->mail['attachments'][] = [
+                'path' => $path,
+                'name' => $name,
+                'encoding' => $encoding,
+                'type' => $type,
+                'disposition' => $disposition
+            ];
+        }
+
+        return $this;
     }
 
     /**
